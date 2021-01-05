402a96ea63a2fb781d01afd6295774eaf198fe8d
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-01-05 13:35:21 +0330

    wrap product schema in its own module

--- apps/api/src/app/product/product.module.ts
+++ apps/api/src/app/product/product.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductController } from './product.controller';
+import { ProductSchema } from './product.model';
 import { ProductService } from './product.service';
 
 @Module({
-  providers: [ProductService]
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
+  controllers: [ProductController],
+  providers: [ProductService],
 })
 export class ProductModule {}
