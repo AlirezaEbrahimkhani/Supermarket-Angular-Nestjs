bb9eee351f3cd8359ddd84ef2f64dc13952d259f
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-01-05 10:40:58 +0330

    connect mongodb

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Alireza:@lirez@1973@supermarket.4piut.mongodb.net/Supermarket?retryWrites=true&w=majority'
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
