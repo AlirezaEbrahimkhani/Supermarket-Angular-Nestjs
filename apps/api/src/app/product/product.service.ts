581504770f43e1ea0d0ada7862d30a00a175a164
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-01-05 13:35:03 +0330

    fetch post and delete method for service and connect to mongodg

--- apps/api/src/app/product/product.service.ts
+++ apps/api/src/app/product/product.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product } from './product.model';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel('Product') private readonly productModel: Model<Product>
+  ) {}
+
+  async insertProduct(
+    title: string,
+    desc: string,
+    price: number
+  ): Promise<string> {
+    const newProduct = new this.productModel({
+      title,
+      description: desc,
+      price,
+    });
+    const result = await newProduct.save();
+    return result.id as string;
+  }
+
+  async getAllProdcuts(): Promise<Product[]> {
+    const products = await this.productModel.find().exec();
+    return products;
+  }
+
+  async deleteProduct(prodId: string) {
+    const result = await this.productModel.deleteOne({ _id: prodId }).exec();
+    if (result.n === 0) {
+      throw new NotFoundException('Could not find product.');
+    }
+  }
+
+  async getSingleProduct(productId: string) {
+    const product = await this.findProduct(productId);
+    return product;
+  }
+
+  private async findProduct(id: string): Promise<Product> {
+    let product;
+    try {
+      product = await this.productModel.findById(id).exec();
+    } catch (error) {
+      throw new NotFoundException('Could not find product.');
+    }
+    if (!product) {
+      throw new NotFoundException('Could not find product.');
+    }
+    return product;
+  }
+}
