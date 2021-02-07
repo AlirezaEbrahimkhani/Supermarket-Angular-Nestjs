82696c44378b4c53c8f59f59eb72240ffb774f97
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-02-07 14:45:06 +0330

    compele auth controller routes

--- apps/api/src/app/auth/auth.controller.ts
+++ apps/api/src/app/auth/auth.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ResponseData } from '../shared/response.model';
+import { RegisterDto, LoginDto } from './auth.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/register')
+  async register(
+    @Body() authCredentialsDto: RegisterDto
+  ): Promise<ResponseData> {
+    const responseData = await this.authService.register(authCredentialsDto);
+    return responseData;
+  }
+
+  @Post('/login')
+  async login(@Body() loginDTO: LoginDto) {
+    const responseData = await this.authService.login(loginDTO);
+    return responseData;
+  }
 }
