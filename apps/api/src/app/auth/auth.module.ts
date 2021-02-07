fb46925edfc449db37ee6242607b5517dc8facad
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-02-07 14:44:42 +0330

    install authentication packages

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserSchema } from './user.model';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
