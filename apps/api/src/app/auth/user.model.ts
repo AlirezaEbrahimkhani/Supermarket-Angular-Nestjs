433efd6425e681f2f2b445576c39758d88a76964
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-02-07 18:40:10 +0330

    add realation to models

--- apps/api/src/app/auth/user.model.ts
+++ apps/api/src/app/auth/user.model.ts
@@ -5,6 +5,7 @@ export const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   roleID: { type: Number, required: true }, // 1. Admin  2. Employee  3. Customer
   salt: { type: String, required: true },
+  prodcuts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
 });
 
 export interface User extends mongoose.Document {
--- apps/api/src/app/product/product.model.ts
+++ apps/api/src/app/product/product.model.ts
@@ -4,6 +4,10 @@ export const ProductSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 export interface Product extends mongoose.Document {
