b4af128a35b39cb7c3abab77ccdf00326e721276
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-02-07 14:45:24 +0330

    update user model

---
+++ apps/api/src/app/auth/user.model.ts
@@ -0,0 +1,16 @@
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  roleID: { type: Number, required: true }, // 1. Admin  2. Employee  3. Customer
+  salt: { type: String, required: true },
+});
+
+export interface User extends mongoose.Document {
+  id: string;
+  username: string;
+  password: string;
+  roleID: number;
+  salt: string;
+}
